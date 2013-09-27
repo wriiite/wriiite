567107197599689744e69c1e354d6a08dbd2f9ab
gaspard <gaspard@gmail.com>
2013-09-27 10:48:07 -0300

    local configuration

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'wriiite',
+			'username'  => 'wriiite',
+			'password'  => 'wr111t3', // silliest dev password ever
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
