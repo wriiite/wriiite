c45bb2edc773f8b3cdc59021e1d308b0cdc3642e
JonathanWi <Jonathan@Mac-mini-de-Jonathan.local>
2013-09-30 16:32:28 +0200

    Models Relationships

--- app/models/Book.php
+++ app/models/Book.php
@@ -4,4 +4,12 @@ class Book extends Eloquent {
 
 	protected $table = 'books';
 
+	public function pages() {
+		return $this->hasMany('Page');
+	}
+
+	public function user() {
+		return $this->belongsTo('User');
+	}
+
 }
