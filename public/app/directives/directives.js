b480c3572470658e1e5981d11973084c4b0d8312
gaspard <gaspard@gmail.com>
2013-11-03 22:53:39 -0300

    front: cleaning up some angular controllers and directives

--- public/app/directives/directives.js
+++ public/app/directives/directives.js
@@ -1,20 +1,20 @@
-app.directive('activeLink', ['$location', function(location) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs, controller) {
-            var clazz = attrs.activeLink;
-            var path = $(element).children('a')[0].hash.substring(1)
-            path = path.substring(1); //hack because path does bot return including hashbang
-            scope.location = location;
-            scope.$watch('location.path()', function(newPath) {
-                if (path === newPath) {
-                    element.addClass(clazz);
-                } else {
-                    element.removeClass(clazz);
-                }
-            });
-        }
-
-    };
-
-    }]);
+app.directive('activeLink', [
+	'$location', function(location) {
+		return {
+			restrict: 'A',
+			link: function(scope, element, attrs, controller) {
+				var clazz = attrs.activeLink;
+				var path = $(element).children('a')[0].hash.substring(1)
+				path = path.substring(1); //hack because path does not return including hashbang
+				scope.location = location;
+				scope.$watch('location.path()', function(newPath) {
+					if (path === newPath) {
+						element.addClass(clazz);
+					} else {
+						element.removeClass(clazz);
+					}
+				});
+			}
+		};
+	}
+]);
